5ea1f2f69819ec36f3a09c54df8ddd4d2c247898
Babz-G <barbaramgaynor@gmail.com>
2025-12-12 15:29:03 -0500

    updated UI to match example - restructured cards, customized toggles, adjusted spacing

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,11 +4,17 @@ import Button from "../Components/Button.jsx";
 function Card(props) {
   return (
     <div className="card">
-      <img src={props.logo} alt={props.name} />
-      <h3>{props.name}</h3>
-      <p>{props.description}</p>
-      <Button buttonText="Remove" onClick={props.remove} />
-      <Toggle checked={props.isActive} onChange={props.toggle} />
+      <div className="cardInfo">
+        <img src={props.logo} alt={props.name} />
+        <div>
+          <h3>{props.name}</h3>
+          <p>{props.description}</p>
+        </div>
+      </div>
+      <div className="cardButtons">
+        <Button buttonText="Remove" onClick={props.remove} />
+        <Toggle checked={props.isActive} onChange={props.toggle} />
+      </div>
     </div>
   );
 }
--- src/Components/Toggle.jsx
+++ src/Components/Toggle.jsx
@@ -1,6 +1,15 @@
 import Switch from "react-switch";
 
 function Toggle(props) {
-  return <Switch checked={props.checked} onChange={props.onChange} />;
+  return (
+    <Switch
+      checked={props.checked}
+      onChange={props.onChange}
+      onColor="#df473f"
+      offColor="#888888"
+      uncheckedIcon={false}
+      checkedIcon={false}
+    />
+  );
 }
 export default Toggle;
